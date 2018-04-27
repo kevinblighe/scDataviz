79af0271f021d505af6b14954a5f6cda0b6acf25
Kevin Blighe <kevinblighe@outlook.com>
2018-04-27 16:10:19 +0100

    Delete downsampleByVar.R

--- testing/downsampleByVar.R
+++
@@ -1,8 +0,0 @@
-downsampleByVar <- function(x, varianceFactor=5)
-{
-  variances <- apply(x, 1, var)
-  x <- x[order(variances, decreasing=TRUE),]
-  x <- x[1:round((nrow(x)/varianceFactor),0),]
-  
-  return(x)
-}
