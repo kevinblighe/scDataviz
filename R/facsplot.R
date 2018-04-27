0a0f94261cf3ddb56a414883f9c401ba9d3be264
Kevin Blighe <k.blighe@ucl.ac.uk>
2018-04-27 16:44:02 +0100

    testing

--- R/facsplot.R
+++ R/facsplot.R
@@ -23,10 +23,11 @@ facsplot <- function(marker1, marker2, df, bins=200, main="FACS plot", xlim=xlim
 
 	par=mar=c(5,5,5,5, cex=0.8)
 
-	hexbinplot(y ~ x, data=df, aspect="1", xbins=bins, xlab=marker1, ylab=marker2, xlim=xlim, ylim=ylim, cex.labels=1.0, cex.title=1.0, colramp=rf,
+	hbplot <- hexbinplot(y ~ x, data=df, aspect="1", xbins=bins, xlab=marker1, ylab=marker2, xlim=xlim, ylim=ylim, cex.labels=1.0, cex.title=1.0, colramp=rf,
     		panel=function(x, y, ...)
     		{
         		panel.hexbinplot(x, y, ...)
         		panel.abline(v=c(x1,x2), h=c(y1,y2), col="black", lwd=4, lty=5)
     		})
+	return(hbplot)
 }
