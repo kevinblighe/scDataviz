8579979257ffdd9edef4e52b598926ea0ca34767
Kevin Blighe <kblighe@CLINICALBIO.dlink.com>
2019-10-04 23:09:06 +0100

    minor

--- R/buildModel.R
+++ R/buildModel.R
@@ -1,27 +1,31 @@
 buildModel <- function(
   data,
-  group = NULL,
+  group,
   groupfactors = unique(data$metadata[,group]),
-  cluster1 = NULL,
-  cluster2 = NULL,
+  cluster = NULL,
   markers = colnames(data$expression))
 {
-  modeling <- data$expression[,which(colnames(data$expression) %in% markers)]
+  data$expression <- data$expression[,which(colnames(data$expression) %in% markers)]
 
-  if (!is.null(group1)) {
-    modeling$group <- factor(data$metadata[,group], levels = groupfactors)
+  if (!is.null(cluster)) {
+    data$expression <- data$expression[which(data$nnc == cluster),]
+    data$nnc <- data$nnc[which(data$nnc == cluster),]
+    data$metadata <- data$metadata[which(data$nnc == cluster),]
+  }
 
-    # build a predictive model of OSSTAT
-    f <- as.formula(
-      paste0('group ~ ',
-        paste0(markers, collapse = "+")))
+  modeling <- data.frame(scale(data$expression), group = factor(data$metadata[,group], levels = groupfactors))
 
-    # use step-wise regression with AIC in order to reduce model in size and
-    # select 'best' predictors
-    require(arm)
-    modeling[is.na(modeling)] <- 0
-    null <- bayesglm(group ~ 1, data = modeling, family = binomial(link = 'logit'))
-    full <- bayesglm(f, data = modeling, family = binomial(link = 'logit'))
+  # build a predictive model of OSSTAT
+  f <- as.formula(
+    paste0('group ~ ',
+      paste0(markers, collapse = "+")))
+
+  # use step-wise regression with AIC in order to reduce model in size and
+  # select 'best' predictors
+  require(arm)
+  modeling[is.na(modeling)] <- 0
+  null <- bayesglm(group ~ 1, data = modeling, family = binomial(link = 'logit'))
+  full <- bayesglm(f, data = modeling, family = binomial(link = 'logit'))
 
   require(MASS)
   forward <- stepAIC(null, scope=list(lower=null, upper=full),
@@ -34,29 +38,14 @@ buildModel <- function(
   # remove markers with unusual error and/or odds ratio CIs
   anova(full, test="Chisq")
   anova(both, test="Chisq")
-  #both <- update(both, . ~ . - monopoiesis_HLA)
   anova(forward, test="Chisq")
-  #forward <- update(forward, . ~ .- monopoiesis_HLA)
   anova(backward, test="Chisq")
-  #backward <- update(backward, . ~ . - events_45_ - events_gran - events_45_1 - monopoiesis_HLA - monopoiesis_HLA_Lympho - myeloid_progenitors_CD117 - erythropoiesis_MFI_CD105_CD105_Ery - erythropoiesis_CD235a)
 
   # write ouy tables
   a <- data.frame(rownames(coefficients(summary(full))), coefficients(summary(full)), exp(cbind("Odds ratio" = coef(full), confint.default(full, level = 0.90))))[-1,]
   colnames(a) <- c("Marker", "Beta", "Std. Error", "Z value", "P value", "OR", "Lower CI", "Upper CI")
   a$Marker <- gsub("_", " ", a$Marker)
   write.table(a, "FullModel.tsv", sep = "\t", row.names = FALSE, quote = FALSE, dec = ".")
-  a <- data.frame(rownames(coefficients(summary(both))), coefficients(summary(both)), exp(cbind("Odds ratio" = coef(both), confint.default(both, level = 0.90))))[-1,]
-  colnames(a) <- c("Marker", "Beta", "Std. Error", "Z value", "P value", "OR", "Lower CI", "Upper CI")
-  a$Marker <- gsub("_", " ", a$Marker)
-  write.table(a, "Model1.tsv", sep = "\t", row.names = FALSE, quote = FALSE, dec = ".")
-  a <- data.frame(rownames(coefficients(summary(forward))), coefficients(summary(forward)), exp(cbind("Odds ratio" = coef(forward), confint.default(forward, level = 0.90))))[-1,]
-  colnames(a) <- c("Marker", "Beta", "Std. Error", "Z value", "P value", "OR", "Lower CI", "Upper CI")
-  a$Marker <- gsub("_", " ", a$Marker)
-  write.table(a, "Model2.tsv", sep = "\t", row.names = FALSE, quote = FALSE, dec = ".")
-  a <- data.frame(rownames(coefficients(summary(backward))), coefficients(summary(backward)), exp(cbind("Odds ratio" = coef(backward), confint.default(backward, level = 0.90))))[-1,]
-  colnames(a) <- c("Marker", "Beta", "Std. Error", "Z value", "P value", "OR", "Lower CI", "Upper CI")
-  a$Marker <- gsub("_", " ", a$Marker)
-  write.table(a, "Model3.tsv", sep = "\t", row.names = FALSE, quote = FALSE, dec = ".")
 
   # perform 10-fold cross-validation on final models to test their utility
   require(boot)
@@ -70,7 +59,7 @@ buildModel <- function(
     par(mar = c(5,5,5,5))
 
     require(pROC)
-    roc <- roc(modeling$Sample, fitted(full), smooth = FALSE)
+    roc <- roc(modeling$group, fitted(full), smooth = FALSE)
     plot.roc(roc,
       grid = TRUE,
       grid.lwd = 2,
